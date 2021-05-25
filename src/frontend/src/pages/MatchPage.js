ad124d8aeb358a3fe8efff5495d15994380c5e7d
mishrasub <subhash06582@gmail.com>
2021-05-25 10:36:40 +0530

    frontend : Match Cmponent and match API integration

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,11 +1,29 @@
 import { React, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { MatchDetailCard } from '../components/MatchDetailCard'
 
 export const MatchPage = () => {
+
+    const [matches, setMatches] = useState([]);
+
+    const { teamName, year } = useParams();
+
+    useEffect(
+        () => {
+            const fetchMatches = async () => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const data = await response.json();
+                console.log(data);
+                setMatches(data);
+            };
+            fetchMatches();
+        }, []
+    );
 
     return (
         <div className="MatchPage">
             <h1>Match Page</h1>
+            {matches?.map(match => <MatchDetailCard key={match.id} teamName={teamName} match={match} />)}
         </div>
     );
 }
