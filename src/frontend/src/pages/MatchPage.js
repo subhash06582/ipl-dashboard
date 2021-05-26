b64f59ea24a18dbda2a7d6d86eb27614aaffa651
mishrasub <subhash06582@gmail.com>
2021-05-26 16:19:42 +0530

    Match Page year selector

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,6 +1,8 @@
 import { React, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { MatchDetailCard } from '../components/MatchDetailCard'
+import { YearSelector } from '../components/YearSelector';
+import './MatchPage.scss';
 
 export const MatchPage = () => {
 
@@ -17,13 +19,19 @@ export const MatchPage = () => {
                 setMatches(data);
             };
             fetchMatches();
-        }, []
+        }, [teamName, year]
     );
 
     return (
         <div className="MatchPage">
-            <h1>Match Page</h1>
-            {matches?.map(match => <MatchDetailCard key={match.id} teamName={teamName} match={match} />)}
+            <div className="year-selector">
+                <h3>Select year</h3>
+                <YearSelector teamName={teamName} />
+            </div>
+            <div>
+                <h1 className="page-heading">{teamName} matches in {year}</h1>
+                {matches?.map(match => <MatchDetailCard key={match.id} teamName={teamName} match={match} />)}
+            </div>
         </div>
     );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,6 +1,6 @@
 import './TeamPage.scss';
 import { React, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { MatchDetailCard } from '../components/MatchDetailCard'
 import { MatchSmallCard } from '../components/MatchSmallCard'
 import { PieChart } from 'react-minimal-pie-chart';
@@ -52,8 +52,8 @@ export const TeamPage = () => {
                     <MatchSmallCard teamName={team.teamName} match={match} />
                 </div>)}
             <div className="more-link">
-                <a href="#"> More ></a>
+                <Link to={`/teams/${teamName}/matches/${process.env.REACT_APP_DATA_END_YEAR}`}> More ></Link>
             </div>
-        </div>
+        </div >
     );
 }
